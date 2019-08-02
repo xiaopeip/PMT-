78f78a01e5e01f8898500f660e9ee715bdb97349
xiaopeip <994080518@qq.com>
2019-08-02 14:05:21 +0800

    修改格式转换程序

--- transhdf.py
+++ transhdf.py
@@ -1,17 +1,17 @@
 import h5py
 import numpy as np
 
-with h5py.File("pgan.h5") as ipt:
+with h5py.File("jieguo/pgan.h5") as ipt:
     hg=ipt["Answer"][()]
 
 leng=len(hg)
-newans=np.empty(leng,dtype=[('EventID',np.int16),('ChannelID',np.int16),('PETime',np.float16),('Weight',np.int8)])
+newans=np.empty(leng,dtype=[('EventID',np.int16),('ChannelID',np.int16),('PETime',np.int16),('Weight',np.float32)])
 
 newans['EventID']=hg[:,0]
 newans['ChannelID']=hg[:,1]
 newans['PETime']=hg[:,2]
 newans['Weight']=hg[:,3]
-print(newans.dtype)
-with h5py.File("pgtest.h5","w") as opt:
+print(newans)
+with h5py.File("pgtestnew.h5","w") as opt:
     opt.create_dataset("Answer",data=newans)
 
