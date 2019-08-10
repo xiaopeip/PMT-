d5d76fb5b10fbf80bd9b339192995aa31edce299
xiaopeip <994080518@qq.com>
2019-08-11 01:01:18 +0800

    使用命令行参数，增加注释

--- read.py
+++ read.py
@@ -4,25 +4,26 @@ import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 from matplotlib import pyplot as plt
+import sys
+args=int(sys.argv[1])
 
 # Read hdf5 file
-h5file = tables.open_file("./data/atraining-0.h5", "r")
+h5file = tables.open_file(f"./data/atraining-{args}.h5", "r")
 WaveformTable = h5file.root.Waveform
 GroundTruthTable = h5file.root.GroundTruth
 
 sinevet,sinchan,sintime=[],[],[]
 
+#根据groundtruth找出只有单光子的事例
 i=1
-while i <10000:
-    
+while i <100000:
     if GroundTruthTable[i]['ChannelID']!=GroundTruthTable[i-1]['ChannelID'] and GroundTruthTable[i]['ChannelID']!=GroundTruthTable[i+1]['ChannelID']:
         sinevet.append(GroundTruthTable[i]['EventID'])
         sintime.append(GroundTruthTable[i]['PETime'])
         sinchan.append(GroundTruthTable[i]['ChannelID'])
     i+=1
 
-
-
+#将单光子事例波形累加
 sumwave=np.zeros(1029,dtype=np.int32)
 sinlen=len(sinevet)
 for x in range(sinlen):
@@ -33,21 +34,15 @@ for x in range(sinlen):
         posi+=1
     sumwave+=np.append(WaveformTable[posi]['Waveform'][sintime[x]:],WaveformTable[posi]['Waveform'][:sintime[x]])-972
 
+#求得平均值
 averwave=sumwave/sinlen
-print(min(sumwave))
-print(sumwave.dtype)
-print(averwave.dtype)
 averzero=np.average(averwave[100:])
 spe=averwave-averzero
 
-with open("average.csv", "w") as opt1:
-    np.savetxt(opt1, np.array([averzero]))
-with h5py.File('singlewave.h5',"w") as opt2:
+with h5py.File(f"medium/average{args+1}.h5", "w") as opt1:
+    opt1.create_dataset("averzero", data=np.array([averzero]))
+with h5py.File(f'medium/singlewave{args+1}.h5',"w") as opt2:
     opt2.create_dataset("spe",data=spe,compression="gzip", shuffle=True)
-
-plt.plot(spe)
-plt.xlabel('Time [ns]')
-plt.ylabel('Voltage [ADC]')
-plt.savefig("singlewave.png")
+#写入文件
 
 h5file.close()
